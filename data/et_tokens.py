f0ddb3deafb1599e20262e7ae022ed9dea6602e3
Anton Yip <anton_yip@hotmail.com>
2022-02-28 18:36:48 +0800

    missing tokens

--- data/et_tokens.py
+++ data/et_tokens.py
@@ -20,3 +20,8 @@ for lpPool in data["mainnet"]:
 
 ## Additional tables -- if you need it 
 print('terra1dj2cj02zak0nvwy3uj9r9dhhxhdwxnw6psse6p,"NIO Inc.","mNIO",6')
+print('terra1qsnj5gvq8rgs7yws8x5u02gwd5wvtu4tks0hjm,"Coca-Cola","mKO",6')
+print('terra15k5r9r8dl8r7xlr29pry8a9w7sghehcnv5mgp6,"LUNAVERSE","LUV",6')
+print('terra15tztd7v9cmv0rhyh37g843j8vfuzp8kw0k5lqv,"altered","ALTE",6')
+print('terra1kkyyh7vganlpkj0gkc2rfmhy858ma4rtwywe3x,"USD Coin (PoS) (Wormhole)","USDC",6')
+print('terra1pvel56a2hs93yd429pzv9zp5aptcjg5ulhkz7w,"USD Coin (Wormhole)","USDC.e",6')
